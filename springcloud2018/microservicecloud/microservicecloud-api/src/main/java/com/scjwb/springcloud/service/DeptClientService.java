0e89e46b114a71648866498bd4cb772b4d7c29c1
suncj <553686853@qq.com>
2018-11-28 18:52:35 +0800

    孙晨杰提交：springcloud调用微服务，添加hystrixDashboard服务监控，zuul网关，服务降级；

--- springcloud2018/microservicecloud/microservicecloud-api/src/main/java/com/scjwb/springcloud/service/DeptClientService.java
+++ springcloud2018/microservicecloud/microservicecloud-api/src/main/java/com/scjwb/springcloud/service/DeptClientService.java
@@ -20,8 +20,8 @@ import com.scjwb.springcloud.entities.Dept;
  * @author zzyy
  * @date 2018年4月21日
  */
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
-//@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService
 {
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
